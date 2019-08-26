a9f305de60d2160ac2cce114a0e67b5d719715e9
Eric <>
2019-08-26 16:39:46 +0800

    ClickForce iOS PreRoll Sample Project Update
    Update Google IMA Player & Mofiy VAST Request URL(Holmesmind).

--- GoogleInteractiveMediaAds.framework/Headers/GoogleInteractiveMediaAds.h
+++ GoogleInteractiveMediaAds.framework/Headers/GoogleInteractiveMediaAds.h
@@ -8,10 +8,14 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-//! Project version number for GoogleInteractiveMediaAds.
+/**
+ * Project version number for GoogleInteractiveMediaAds.
+ */
 FOUNDATION_EXPORT double GoogleInteractiveMediaAdsVersionNumber;
 
-//! Project version string for GoogleInteractiveMediaAds.
+/**
+ * Project version string for GoogleInteractiveMediaAds.
+ */
 FOUNDATION_EXPORT const unsigned char GoogleInteractiveMediaAdsVersionString[];
 
 #if __IPHONE_OS_VERSION_MIN_REQUIRED < __IPHONE_8_0
@@ -39,5 +43,6 @@ FOUNDATION_EXPORT const unsigned char GoogleInteractiveMediaAdsVersionString[];
 #import <GoogleInteractiveMediaAds/IMAStreamManager.h>
 #import <GoogleInteractiveMediaAds/IMAStreamRequest.h>
 #import <GoogleInteractiveMediaAds/IMAUiElements.h>
+#import <GoogleInteractiveMediaAds/IMAUniversalAdID.h>
 #import <GoogleInteractiveMediaAds/IMAVODStreamRequest.h>
 #import <GoogleInteractiveMediaAds/IMAVideoDisplay.h>
--- GoogleInteractiveMediaAds.framework/Headers/IMAStreamManager.h
+++ GoogleInteractiveMediaAds.framework/Headers/IMAStreamManager.h
@@ -59,6 +59,7 @@
  */
 - (void)streamManager:(IMAStreamManager *)streamManager didReceiveAdError:(IMAAdError *)error;
 
+@optional
 
 /**
  *  Called when the ad is playing to give updates about ad progress.
@@ -136,6 +137,9 @@
  */
 - (IMACuepoint *)previousCuepointForStreamTime:(NSTimeInterval)streamTime;
 
+/**
+ * :nodoc:
+ */
 - (instancetype)init NS_UNAVAILABLE;
 
 /**
